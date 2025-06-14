2d460276146861e7f0c072aa4d0aa07161f7b924
Darky <179386711+Darky-Github@users.noreply.github.com>
2025-06-14 23:21:19 +0530

    Update types.py

--- simplescript/types.py
+++ simplescript/types.py
@@ -1,1 +1,22 @@
+from enum import Enum
 
+class TokenType(Enum):
+    KEYWORD = "KEYWORD"
+    IDENTIFIER = "IDENTIFIER"
+    TYPE = "TYPE"
+    OPERATOR = "OPERATOR"
+    STRING = "STRING"
+    NUMBER = "NUMBER"
+    BOOLEAN = "BOOLEAN"
+    CHARACTER = "CHARACTER"
+    SYMBOL = "SYMBOL"
+    ASSIGN = "ASSIGN"
+    EOF = "EOF"
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.type.name}:{self.value}"
